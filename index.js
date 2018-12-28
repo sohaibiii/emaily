edecfdbc443f377d4278ec9dd9a03a81f86987e0
sohaibiii <bsaif281@gmail.com>
2018-12-28 15:32:39 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const app = require('express')()
+
+const port = process.env.PORT || 5000
+app.get('/', (req, res) => {
+  res.send({ name: 'sohaib' })
+})
+
+app.listen(port, () => {
+  console.log(`server has been established at ${post}`)
+})
