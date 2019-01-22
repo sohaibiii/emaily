8fd3977be0c87bee81489da890b7f61447518259
sohaibiii <bsaif281@gmail.com>
2019-01-22 09:24:12 +0500

    my

--- dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  clientID:
-    '997670496101-g3r1ctso15s7tppmiunubmntl6ogism5.apps.googleusercontent.com',
-  clientSecret: 'abxTN5kNdnFI7leP9Ukvdd9E',
-  MONGODB_URI: 'mongodb://sohaib:alibhaiwow9@ds145574.mlab.com:45574/emaily',
-  keys: 'jsfdhjgsdfgsdjfhsgfjsgdhfsgdjgsjfsd'
-}
